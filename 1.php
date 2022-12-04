0f4a40cc1b10c607a02e998a50aae346bb0b2043
solanki-parin <solankiparin78@gmail.com>
2022-12-04 22:04:54 +0530

    cpp

--- 1.php
+++
@@ -1,7 +0,0 @@
-<?php
-echo "<pre>";
-echo "<br>";
-print_r(getenv());
-//fieldset and legend for box with title
-
-?>
--- delete.php
+++
@@ -1,8 +0,0 @@
-<?php
-$db=mysqli_connect("localhost","root","","first");
-$id=$_REQUEST['id'];
-$del="DELETE FROM `data` WHERE `id`='$id'";
-$delete=mysqli_query($db,$del);
-header("location:delquery.php");
-
-?>
